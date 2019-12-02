1ea54cb04a60f45f8f25f2ac268f84d7ba5b9ebc
JONATHAN <jonn.thinkrtc@gmail.com>
2019-12-02 12:36:04 -0500

    product detail modificiations

--- core/views.py
+++ core/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Item
-from django.views.generic import TemplateView, ListView
-
+from django.views.generic import TemplateView, ListView, DetailView
 # Create your views here.
 
 class HomeView(ListView):
     model = Item
     template_name = "home.html"
+
+
+class ProductDetailView(DetailView):
+    model = Item
+    template_name = "product_detail.html"
+
 
 def products(request):
     context = {
