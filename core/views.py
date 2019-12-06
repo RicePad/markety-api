2a689537ce02c8149d248653133b2c19da0e64af
JONATHAN <jonn.thinkrtc@gmail.com>
2019-12-06 14:08:19 -0500

    remove_single_item_from_cart

--- core/views.py
+++ core/views.py
@@ -67,3 +67,39 @@ def remove_from_cart(request, slug):
 
 
 
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+
+    if order_qs.exists():
+        order = order_qs[0]
+        #check to see if the order item is in the order
+
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+
+            if order_item.quantity > 1:
+                order_item.quantity -=1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "this item quantity was updated")
+            return redirect("core:product-detail", slug=slug)
+        else:
+            messages.info(request, "this item was not in your cart")
+            return redirect("core:product-detail", slug=slug)
+    else:
+         messages.info(request, "you do not have an active order")
+         return redirect("core:product-detail", slug=slug)
+
+            
+        
+
+
