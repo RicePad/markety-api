6c452847ad6db819c4e9cbb2f17bce1f5d3354ac
JONATHAN <jonn.thinkrtc@gmail.com>
2019-12-10 16:03:18 -0500

    cart_item_count template tag

--- core/views.py
+++ core/views.py
@@ -19,7 +19,7 @@ class ItemDetailView(DetailView):
     template_name = "product_detail.html"
 
 class OrderSummaryView(View):
-    pass
+    template_name = "order_summary.html"
 
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
@@ -31,21 +31,23 @@ def add_to_cart(request, slug):
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
-        #check if the order item is in the order
+        # check if the order item is in the order
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
-            messages.info(request, "this item quantity was updated.")
+            messages.info(request, "This item quantity was updated.")
             return redirect("core:home")
         else:
             order.items.add(order_item)
-            messages.info(request, "this item was added to your cart.")
-            return redirect
+            messages.info(request, "This item was added to your cart.")
+            return redirect("core:home")
     else:
         ordered_date = timezone.now()
-        order = Order.objects.create( user=request.user, ordered_date=ordered_date)
-        messages.info(request, "This item was added to your cart")
-        return redirect("core:home")
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+        messages.info(request, "This item was added to your cart.")
+        return redirect("core:order-summary")
 
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
