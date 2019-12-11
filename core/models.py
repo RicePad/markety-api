a8b556a643f3a36e1f83b221cfa26068628ae366
JONATHAN <jonn.thinkrtc@gmail.com>
2019-12-11 15:10:55 -0500

    order-summary UI

--- core/models.py
+++ core/models.py
@@ -58,6 +58,11 @@ class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_total_discount_item_price(self):
+        return self.quantity * self.item.discount_price
 
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
