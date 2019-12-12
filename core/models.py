0faff2774bdabaf0beba0b5691d700bdf4f9d2dc
JONATHAN <jonn.thinkrtc@gmail.com>
2019-12-12 15:21:59 -0500

    order-summary UI

--- core/models.py
+++ core/models.py
@@ -58,6 +58,9 @@ class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
     def get_total_item_price(self):
         return self.quantity * self.item.price
 
@@ -69,10 +72,15 @@ class OrderItem(models.Model):
             'slug': self.slug
         })
     
-    def __str__(self):
-        return f"{self.quantity} of {self.item.title}"
-    
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
 
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
+
+    
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=(""), on_delete=models.CASCADE)
     items = models.ManyToManyField(OrderItem, verbose_name=(""))
@@ -82,4 +90,10 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
     
