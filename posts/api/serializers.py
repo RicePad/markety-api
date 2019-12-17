ee9cc559eb1da9ac144fcd80613b3c3504502aaa
JON <jonn.thinkrtc@gmail.com>
2019-12-17 14:31:52 -0500

    remove post app

--- posts/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from ..models import Post
-from rest_framework import serializers
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('title', 'content', 'is_featured')
--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .api import views
-
-urlpatterns = [
-    path('', views.PostListView.as_view(), name=None),
-    path('create/', views.PostCreateView.as_view(), name=None),
-    path('<int:pk>/', views.PostDetailView.as_view(), name=None),
-]
